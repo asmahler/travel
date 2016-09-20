52a57bcfcc9280f74db56664441e6326013bbf56
am <asmahler@gmail.com>
2016-09-19 23:22:18 -0700

    added mixins for @media b breaks. added page section classes and wrapper class.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ var autoprefixer = require('autoprefixer'),
  	     postcss = require('gulp-postcss'), 
  	     cssvars = require('postcss-simple-vars'),
  	     nested  = require('postcss-nested'), 
+ 	     mixins  = require('postcss-mixins'),
  	        gulp = require('gulp');
   
  
 gulp.task('styles', function () { 
 	return gulp.src('./app/assets/styles/style.css')
-	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+	.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
 	.on('error', function (errorInfo){
 		console.log(errorInfo.toString());
 		this.emit('end');
