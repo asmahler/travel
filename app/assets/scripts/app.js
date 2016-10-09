40d6ce404c1787fbd768fe1c77489d5cb561b845
am <asmahler@gmail.com>
2016-10-09 15:46:25 -0700

    finsihed reveal on scroll

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/mobile_menu';
+import RevealOnScroll from './modules/reveal';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu(); 
+new RevealOnScroll($('.feature-item'), "85%");
+new RevealOnScroll($('.testimonial'), '60%');
